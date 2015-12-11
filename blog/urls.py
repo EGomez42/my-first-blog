278680871b5225c775d495f67ec3bfce4706a890
Egomez <emmanuelgomezgutierrez>
2015-12-10 17:20:57 -0800

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 	# Si la url tiene -->  r'^$'  Indica una expresion regular
 	# Indica que sera la pagina inicial (home) de nuestro proyecto 
 	url(r'^$', views.post_list, name='post_list'),
+	# Observar post's a detalle
+	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 
 ]
