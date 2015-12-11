dbb6b87855c15c689ecba2093ad3e65bf1cffdac
Egomez <emmanuelgomezgutierrez>
2015-12-11 12:11:56 -0800

    Comments Function and Login Function

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
-
-    # Incluye/Busca/Trae todos los urls de la App blog
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+        # Incluye/Busca/Trae todos los urls de la App blog
     url(r'', include('blog.urls')),
-
 ]
